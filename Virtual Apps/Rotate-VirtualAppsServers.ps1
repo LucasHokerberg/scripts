af6703d400e727a62a83363223c991857a049a4c
Lucas Hökerberg <lucas@hokerberg.com>
2020-09-08 08:55:14 +0200

    Update Rotate-VirtualAppsServers.ps1

--- Virtual Apps/Rotate-VirtualAppsServers.ps1
+++ Virtual Apps/Rotate-VirtualAppsServers.ps1
@@ -1,29 +1,32 @@
-# -------------------------------------------------------------------------------------------------------------
-# Script to "rotate" Virtual Apps servers. I.e. the following:
-# 1. Boot up standby servers and take them out off maintanance mode
-# 2. Put active servers in maintanance mode
-# 3. Inform users on active servers to sign out
-# 4. Shut down active servers
-#
-# After a complete rotation, the script will send out a mail notification.
-#
-# Usage:
-# .\Rotate-VirtualAppsServers.ps1 -Active <Servers> -Standby <Servers> -Delay <Minutes> -Message <Filename>
-#
-# Arguments:
-# Active - List of servers to be powered on and active after the rotation (seperated by comma).
-# Standby - List of servers to shutdown and put as standby after the rotation (separated by comma).
-# Delay - Delay in minutes until the active servers will be rebooted.
-# Message - Relative path to a text file containing an extra message to be sent out to sessions on servers to be set in standby.
-#
-# Notification:
-# The script will send out a message with a standard text, plus any optional text, 3 times periodically during the delay time.
-#
-# Example:
-# .\Rotate-VirtualAppsServers.ps1 -Active VDA01,VDA02 -Standby VDA03,VDA04 -Delay 60 -Message message.txt
-#
-# Author: lucas@hokerberg.com
-# -------------------------------------------------------------------------------------------------------------
+<#
+
+This script "rotates" Virtual Apps servers. I.e. the following:
+1. Boot up standby servers and take them out of maintenance mode
+2. Put active servers in maintenance mode
+3. Inform users on active servers to sign out
+4. Shut down active servers
+
+If a server is not booting up, the script will abort and send out a mail notification.
+After a successfull rotation, the script will send out a mail notification.
+
+Author: lucas@hokerberg.com
+
+Usage:
+.\Rotate-VirtualAppsServers.ps1 -Active <Servers> -Standby <Servers> -Delay <Minutes> -Message <Filename>
+
+Arguments:
+Active - List of servers to be powered on and active after the rotation (seperated by comma).
+Standby - List of servers to shutdown and put as standby after the rotation (separated by comma).
+Delay - Delay in minutes until the active servers will shutdown.
+Message - Relative path to a text file containing an extra message to be sent out to sessions on servers to be set in standby.
+
+Notification:
+The script will send out a message with a standard text, plus any optional text, three times periodically during the delay time.
+
+Example:
+.\Rotate-VirtualAppsServers.ps1 -Active VDA01,VDA02 -Standby VDA03,VDA04 -Delay 60 -Message info.txt
+
+#>
 
 # Fetch parameters
 param (
@@ -33,36 +36,59 @@ param (
     [string] $Message
 )
 
-# Define mail parameters
-$smtpFrom = "VDA@domain.com" # Sender address
-$smtpTo = @("admin@domain.com") # Send mail to (separate multiple recipients with comma)
-$smtpServer = "smtp.domain.local" # Your SMTP server
+# Define static parameters
+$smtpFrom = "no-reply@domain.com" # Sender address
+$smtpTo = @("logs@domain.com") # Send mail to (separate multiple recipients with comma)
+$smtpServer = "smtp.domain.com" # Your SMTP server
 
-# Load modules
+# Load required modules
 Add-PSSnapin Citrix*
 
 # Repeat for each server to be active after the rotation
 foreach ($server in $Active) {
     
-    # Power on
+    # Power on server and wait for registration
+    $i = 0
     New-BrokerHostingPowerAction -MachineName $server -Action TurnOn
-
-    # While the server is not registered
     while ((Get-BrokerMachine -MachineName $env:userdomain\$server).RegistrationState -ne "Registered") {
         
-        # Wait
+        $i++
         Start-Sleep -Seconds 15
+
+        # Abort if server is not registering
+        if ($i -eq 16) {
+            
+            [string[]]$smtpTo = $smtpTo.Split(",")
+            Send-MailMessage -From $smtpFrom -To $smtpTo -Subject "Virtual Apps Server Rotation" -Body "The Virtual Apps server rotation was aborted. The server $server never registered." -SmtpServer $smtpServer
+            exit
+        }
     }
 
-    # Exit maintanance mode
+    # Exit maintenance
     Set-BrokerMachineMaintenanceMode -InputObject $env:userdomain\$server $false
+
+    # Abort if server is still in maintenance mode
+    if (-not (Get-BrokerMachine -MachineName $env:userdomain\$server -InMaintenanceMode $false)) {
+
+        [string[]]$smtpTo = $smtpTo.Split(",")
+        Send-MailMessage -From $smtpFrom -To $smtpTo -Subject "Virtual Apps Server Rotation" -Body "The Virtual Apps server rotation was aborted. The server $server is still in maintenance mode." -SmtpServer $smtpServer
+        exit
+    }
 }
 
 # Repeat for each server to be standby after the rotation
 foreach ($server in $Standby) {
 
-    # Enter maintanance mode
+    # Enter maintenance
     Set-BrokerMachineMaintenanceMode -InputObject $env:userdomain\$server $true
+
+    # Abort if server is still not in maintenance mode
+    if (-not (Get-BrokerMachine -MachineName $env:userdomain\$server -InMaintenanceMode $true)) {
+
+        [string[]]$smtpTo = $smtpTo.Split(",")
+        Send-MailMessage -From $smtpFrom -To $smtpTo -Subject "Virtual Apps Server Rotation" -Body "The Virtual Apps server rotation was aborted. The server $server could not enter maintenance mode." -SmtpServer $smtpServer
+        exit
+    }
 }
 
 # Calculate rotation time and message interval
@@ -89,8 +115,8 @@ foreach ($server in $Standby) {
         # Send message
         Send-BrokerSessionMessage $sessions `
             -MessageStyle Exclamation `
-            -Title "Maintanance window at $($rotateTime.ToString("HH:mm")) (in $([math]::Round(($rotateTime-(Get-Date)).TotalMinutes)) minutes)" `
-            -Text "Warning 1 of 3.`n`n$msg"
+            -Title "Servicefönster kl. $($rotateTime.ToString("HH:mm")) (om $([math]::Round(($rotateTime-(Get-Date)).TotalMinutes)) minuter)" `
+            -Text "Varning 1 av 3.`n`n$msg"
     }
 }
 
@@ -110,8 +136,8 @@ foreach ($server in $Standby) {
         # Send message
         Send-BrokerSessionMessage $sessions `
             -MessageStyle Exclamation `
-            -Title "Maintanance window at $($rotateTime.ToString("HH:mm")) (in $([math]::Round(($rotateTime-(Get-Date)).TotalMinutes)) minutes)" `
-            -Text "Warning 2 of 3.`n`n$msg"
+            -Title "Servicefönster kl. $($rotateTime.ToString("HH:mm")) (om $([math]::Round(($rotateTime-(Get-Date)).TotalMinutes)) minuter)" `
+            -Text "Varning 2 av 3.`n`n$msg"
     }
 }
 
@@ -131,8 +157,8 @@ foreach ($server in $Standby) {
         # Send message
         Send-BrokerSessionMessage $sessions `
             -MessageStyle Exclamation `
-            -Title "Maintanance window at $($rotateTime.ToString("HH:mm")) (in $([math]::Round(($rotateTime-(Get-Date)).TotalMinutes)) minutes)" `
-            -Text "Warning 3 of 3.`n`n$msg"
+            -Title "Servicefönster kl. $($rotateTime.ToString("HH:mm")) (om $([math]::Round(($rotateTime-(Get-Date)).TotalMinutes)) minuter)" `
+            -Text "Varning 3 av 3.`n`n$msg"
     }
 }
 
@@ -149,3 +175,4 @@ foreach ($server in $Standby) {
 # Send email notification
 [string[]]$smtpTo = $smtpTo.Split(",")
 Send-MailMessage -From $smtpFrom -To $smtpTo -Subject "Virtual Apps Server Rotation" -Body "The Virtual Apps server rotation is complete.`n`nNew active servers: $Active`n`nNew standby servers: $Standby`n`nPlease verify!" -SmtpServer $smtpServer
+exit
