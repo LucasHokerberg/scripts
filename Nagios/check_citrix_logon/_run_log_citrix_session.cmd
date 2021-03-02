6968e5d5357d777f61a20bf908cf74b09dc4dc4f
Lucas Hökerberg <lucas@hokerberg.com>
2021-03-02 08:37:05 +0100

    Use private e-mail instead

--- Nagios/check_citrix_logon/_run_log_citrix_session.cmd
+++ Nagios/check_citrix_logon/_run_log_citrix_session.cmd
@@ -4,7 +4,7 @@ rem This script runs the log_citrix_session PowerShell script.
 rem Call this script at logon.
 
 rem Created: 2021-02-17
-rem Author: lucas.hokerberg@gelita.com
+rem Author: lucas@hokerberg.com
 
 powershell.exe -File "C:\Users\username\Desktop\log_citrix_session.ps1"
 
